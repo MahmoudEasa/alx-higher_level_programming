3de8f3f18cc0644f070a9bc088212cd5aecb1bee
MahmoudEasa <mu01011422865@gmail.com>
2023-06-06 15:24:12 +0300

    Fix 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-main.py
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -3,4 +3,5 @@ uppercase = __import__('8-uppercase').uppercase
 
 uppercase("best")
 uppercase("Best School 98 Battery street")
+uppercase("")
 
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def uppercase(str):
     str_len = len(str)
+
+    if str_len == 0:
+        print("\n")
+
     for i, c in enumerate(str):
         end_chr = "\n" if i == str_len - 1 else ""
+        char = c
         if ord(c) in range(ord('a'), ord('z') + 1):
             char = chr(ord(c) - 32)
-            print("{}".format(char), end=end_chr)
-        else:
-            print("{}".format(c), end=end_chr)
+
+        print("{}".format(char), end=end_chr)
