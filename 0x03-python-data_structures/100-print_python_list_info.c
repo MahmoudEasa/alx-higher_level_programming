e8291a69114498ed9b0166e8ab2302daa126da02
MahmoudEasa <mu01011422865@gmail.com>
2023-06-09 19:25:06 +0300

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,29 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints some basic info about Python lists
+ * @p: pointer
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	int len, i;
+	PyObject *item, *type;
+
+	if (!PyList_Check(p))
+		return;
+
+	len = PyList_Size(p);
+
+	printf("[*] Size of the Python List = %d\n", len);
+	printf("[*] Allocated = %d\n", ((PyListObject *)p)->allocated);
+
+	for (i = 0; i < len; i++)
+	{
+		item = PyList_GetItem(p, i);
+		type = PyObject_Type(item);
+		printf("Element %d: %s", i, Py_TYPE(item)->tp_name);
+		Py_DECREF(type);
+	}
+}
+
---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to the first node
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *ptr;
+
+	if (!head || !(*head))
+		return (1);
+
+	return (0);
+}
+
