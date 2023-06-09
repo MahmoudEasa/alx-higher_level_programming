16bd47e248703dff85a65a320238e6289f0b2b6a
MahmoudEasa <mu01011422865@gmail.com>
2023-06-09 15:49:09 +0300

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    list_len = len(my_list)
+
+    if idx < 0 or idx > list_len:
+        return (None)
+
+    for i in range(list_len):
+        if i == idx:
+            return (my_list[i])
