0d5734352910cf7e4046bdc2ba3aab525d4c2052
MahmoudEasa <mu01011422865@gmail.com>
2023-06-10 18:39:19 +0300

    13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,8 @@
-#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
 
+listint_t *push(listint_t **head, int n);
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to the first node
@@ -9,11 +12,58 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *ptr;
+	listint_t *current, *rev = NULL, *current_rev;
 
 	if (!head || !(*head))
 		return (1);
 
-	return (0);
+	current = *head;
+
+	while (current)
+	{
+		push(&rev, current->n);
+		current = current->next;
+	}
+
+	current = *head;
+	current_rev = rev;
+
+	while ((current && current_rev))
+	{
+		if (current->n != current_rev->n)
+		{
+			free_listint(rev);
+			return (0);
+		}
+		current = current->next;
+		current_rev = current_rev->next;
+	}
+
+	free_listint(rev);
+
+	return (1);
+}
+
+/**
+ * push - add node to start
+ * @head: the head of the node
+ * @n: intger
+ *
+ * Return: pointer to new node
+ */
+
+listint_t *push(listint_t **head, int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
 
