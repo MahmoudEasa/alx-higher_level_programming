6734c218194d2a44ebdba9ca0a0130ca44f50857
MahmoudEasa <mu01011422865@gmail.com>
2023-06-26 21:28:11 +0300

    Fix 4-list_division.py 1

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    try:
-        for i in range(list_length):
-            if type(my_list_1[i]) in [int, float]\
-                    and type(my_list_2[i]) in [int, float]:
-                if my_list_1[i] <= 0 or my_list_2[i] <= 0:
-                    print("division by 0")
-                    new_list.append(0)
+    if type(my_list_1) == list and type(my_list_2) == list:
+        try:
+            for i in range(list_length):
+                if type(my_list_1[i]) in [int, float]\
+                        and type(my_list_2[i]) in [int, float]:
+                    if my_list_1[i] <= 0 or my_list_2[i] <= 0:
+                        print("division by 0")
+                        new_list.append(0)
+                    else:
+                        new_list.append(my_list_1[i] / my_list_2[i])
                 else:
-                    new_list.append(my_list_1[i] / my_list_2[i])
-            else:
-                print("wrong type")
-                new_list.append(0)
-    except IndexError:
-        list_1_len = len(my_list_1)
-        list_2_len = len(my_list_2)
-        if list_1_len > i:
-            while i < list_1_len:
-                new_list.append(0)
-                i += 1
-        if list_2_len > i:
-            while i < list_2_len:
-                new_list.append(0)
-                i += 1
-        print("out of range")
-    finally:
-        return (new_list)
+                    print("wrong type")
+                    new_list.append(0)
+        except IndexError:
+            list_1_len = len(my_list_1)
+            list_2_len = len(my_list_2)
+            if list_1_len > i:
+                while i < list_1_len:
+                    new_list.append(0)
+                    i += 1
+            if list_2_len > i:
+                while i < list_2_len:
+                    new_list.append(0)
+                    i += 1
+            print("out of range")
+        finally:
+            return (new_list)
