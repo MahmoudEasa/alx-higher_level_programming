718051e3e355715bc94758103a38ef0ac49dc6a4
MahmoudEasa <mu01011422865@gmail.com>
2023-06-27 21:06:47 +0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""Class Square that defines a square"""
+
+
+class Square:
+    """class Square
+
+    """
+    def __init__(self, size):
+        """The Initial Attributes
+
+            Attributes:
+                size (int): Private instance attribute
+        """
+
+        self.__size = size
