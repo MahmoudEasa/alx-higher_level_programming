c27d25f24f4048c4a95bd43d8eb6738ceea0c206
MahmoudEasa <mu01011422865@gmail.com>
2023-06-30 03:32:15 +0300

    Fix 103-magic_class.py 2

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
+"""Class MagicClass that does Python bytecode"""
 import math
-
-"""Class MagicClass that does Python bytecode
-"""
 
 
 class MagicClass:
     """Class does Python bytecode"""
     def __init__(self, radius=0):
+    """Initial Data"""
         if type(radius) is not int or type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
         """Return area"""
-        return((self.__radius ** 2) * math.pi)
+        return ((self.__radius ** 2) * math.pi)
 
     def circumference(self):
         """Return circumference"""
-        return(2 * math.pi * self.__radius)
+        return (2 * math.pi * self.__radius)
