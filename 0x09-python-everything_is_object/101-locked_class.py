4de8f6a7e13f149bb8c201b39a37a1daa912793e
MahmoudEasa <mu01011422865@gmail.com>
2023-07-04 21:41:12 +0300

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class LockedClass:
+    __slots__ = ['first_name']
+
+    def __setattr__(self, key, value):
+        if key != 'first_name':
+            raise AttributeError(f"'LockedClass'\
+ object has no attribute '{key}'")
+        super().__setattr__(key, value)
