6e0eb720fd47cef3597bf2c71569db148a7e34a5
MahmoudEasa <mu01011422865@gmail.com>
2023-07-05 12:37:22 +0300

    Fix 0-add_integer.py 3

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,11 +14,11 @@ def add_integer(a, b=98):
 
     if type(a) not in [int, float] or\
         a == float('inf') or a == -float('inf')\
-            or a == float('nan'):
+            or a != a:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float] or\
         b == float('inf') or b == -float('inf')\
-            or b == float('nan'):
+            or b != b:
         raise TypeError("b must be an integer")
 
     return (int(a) + int(b))
