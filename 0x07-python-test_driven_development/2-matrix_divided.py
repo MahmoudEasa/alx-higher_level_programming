c49fa8ed6f84a5f8b1b86d9ab60bd23e5f2d3471
MahmoudEasa <mu01011422865@gmail.com>
2023-07-05 13:13:37 +0300

    Fix 2-matrix_divided.py 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,15 +7,21 @@
 def matrix_divided(matrix, div):
     """Function to divides all elements of a mattix
     """
+
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0 or div == float('inf'):
+        raise ZeroDivisionError("division by zero")
+
     new_matrix = []
     if type(matrix) != list or len(matrix) == 0:
-        raise TypeError("Matrix must be a matrix\
+        raise TypeError("matrix must be a matrix\
  (list of lists) of integers/floats")
 
     row_size = len(matrix[0])
     for r in matrix:
         if type(r) != list or len(r) == 0:
-            raise TypeError("Matrix must be a matrix\
+            raise TypeError("matrix must be a matrix\
  (list of lists) of integers/floats")
         if len(r) != row_size:
             raise TypeError("Each row of the matrix must have the same size")
@@ -23,13 +29,8 @@ def matrix_divided(matrix, div):
         col = []
         for c in r:
             if type(c) not in [int, float]:
-                raise TypeError("Matrix must be a matrix\
+                raise TypeError("matrix must be a matrix\
  (list of lists) of integers/floats")
-
-            if type(div) not in [int, float]:
-                raise TypeError("div must be a number")
-            if div == 0 or div == float('inf'):
-                raise ZeroDivisionError("division by zero")
 
             col.append(round(c / div, 2))
         new_matrix.append(col)
