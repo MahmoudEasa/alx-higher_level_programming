89bee3f269cb8973db84560f8d7c0a853ce97dac
MahmoudEasa <mu01011422865@gmail.com>
2023-07-08 02:10:53 +0300

    Fix 101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
-import numpy as np
-
 """Module to multiplies 2 matrices by using the module NumPy
+
+
+
 """
 
+
+import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
     """Function to multiplies 2 matrices by using the module NumPy
