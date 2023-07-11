c935c409184d3a7aaa7fdeeb8f5940f04388bf4b
MahmoudEasa <mu01011422865@gmail.com>
2023-07-11 19:35:26 +0000

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Module to Write a class Student that defines a student
+"""
+
+
+class Student:
+    """Class Student
+
+        Attributes:
+            first_name (str): The first name
+            last_name (str): The last name
+            age (int): The age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        student = {}
+        if type(attrs) == list and len(attrs) > 0:
+            for arg in attrs:
+                if arg in list(self.__dict__.keys()):
+                    student[arg] = self.__dict__[arg]
+        else:
+            student = {
+                        'first_name': self.first_name,
+                        'last_name': self.last_name,
+                        'age': self.age
+                    }
+        return (student)
