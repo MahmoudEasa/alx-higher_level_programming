02494db28e233882577c7b9f711887c9366c73c6
MahmoudEasa <mu01011422865@gmail.com>
2023-07-11 15:16:09 +0000

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module to reads a text file (UTF8) and prints it to stdout
+
+"""
+
+
+def read_file(filename=""):
+    """Module to reads a text file (UTF8) and prints it to stdout
+
+        Args:
+            filename (str): the file name
+    """
+
+    with open(filename, 'r') as f:
+        for line in f:
+            print(line, end="")
