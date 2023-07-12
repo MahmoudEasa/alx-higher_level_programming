e8ac6700615f014417ef4d3f2a849f2538475b97
MahmoudEasa <mu01011422865@gmail.com>
2023-07-12 14:28:09 +0000

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -9,21 +9,7 @@ prints the following statistics:
 """
 
 
-file_size = 0
-lines = 0
-status_obj = {
-            200: 0,
-            301: 0,
-            400: 0,
-            401: 0,
-            403: 0,
-            404: 0,
-            405: 0,
-            500: 0,
-        }
-
-
-def print_status():
+def print_status(file_size, status_obj):
     """Function to print result
 
     """
@@ -33,20 +19,38 @@ def print_status():
             print(f"{status}: {status_obj[status]}")
 
 
-try:
-    while True:
-        input_str = input()
-        lines += 1
-        if lines == 10:
-            print_status()
-            lines = 1
-        if len(input_str):
-            inputs = input_str.split(" ")
-            file_size += int(inputs[-1])
-            st = int(inputs[-2])
-            for status in list(status_obj.keys()):
-                if st == status:
-                    status_obj[status] += 1
-                    break
-except KeyboardInterrupt:
-    print_status()
+def main():
+    file_size = 0
+    lines = 0
+    status_obj = {
+                200: 0,
+                301: 0,
+                400: 0,
+                401: 0,
+                403: 0,
+                404: 0,
+                405: 0,
+                500: 0,
+            }
+
+    try:
+        while True:
+            input_str = input()
+            lines += 1
+            if lines == 10:
+                print_status(file_size, status_obj)
+                lines = 1
+            if len(input_str):
+                inputs = input_str.split(" ")
+                file_size += int(inputs[-1])
+                st = int(inputs[-2])
+                for status in list(status_obj.keys()):
+                    if st == status:
+                        status_obj[status] += 1
+                        break
+    except KeyboardInterrupt:
+        print_status(file_size, status_obj)
+
+
+if __name__ == "__main__":
+    main()
