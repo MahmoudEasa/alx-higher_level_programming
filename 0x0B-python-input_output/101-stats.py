12f7df20b73cba7b4e85370863b7079e42237dee
MahmoudEasa <mu01011422865@gmail.com>
2023-07-12 14:14:57 +0000

    Fix 101-stats.py 6

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
 
-"""Module to reads
-    stdin line by line and computes metrics
+"""Module to reads stdin line by line and computes metrics
 
-    Input format: <IP Address> - [<date>]
-        "GET /projects/260 HTTP/1.1"
-        <status code> <file size>
-    Each 10 lines and after a keyboard interruption (CTRL + C),
-        prints those statistics since the beginning:
-    Total file size: File size: <total size>
-    where is the sum of all previous (see input format above)
-    Number of lines by status code:
-    possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-    if a status code doesn’t appear,
-        don’t print anything for this status code
-    format: <status code>: <number>
-    status codes should be printed in ascending order
-
+After every ten lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
 
 
