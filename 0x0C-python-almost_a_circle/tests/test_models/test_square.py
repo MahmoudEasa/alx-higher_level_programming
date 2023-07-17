dea9af298a7a7099ff7a332ec5d51b809fabeb72
MahmoudEasa <mu01011422865@gmail.com>
2023-07-17 18:47:36 +0000

    Fix tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -17,8 +17,45 @@ class TestSquare(unittest.TestCase):
     def test_square(self):
         """Test square
         """
-        r1 = Square(10, 2, 0, 5)
-        self.assertEqual(r1.id, 5)
+        r1 = Square(5)
+        res = "[Square] (20) 0/0 - 5\n"
+        with patch('sys.stdout', new=StringIO()) as str_out:
+            print(r1)
+        self.assertEqual(str_out.getvalue(), res)
+
+        self.assertEqual(r1.area(), 25)
+
+        res = "#####\n#####\n#####\n#####\n#####\n"
+        with patch('sys.stdout', new=StringIO()) as str_out:
+            r1.display()
+        self.assertEqual(str_out.getvalue(), res)
+
+        r1 = Square(2, 2)
+        res = "[Square] (21) 2/0 - 2\n"
+        with patch('sys.stdout', new=StringIO()) as str_out:
+            print(r1)
+        self.assertEqual(str_out.getvalue(), res)
+
+        self.assertEqual(r1.area(), 4)
+
+        res = "  ##\n  ##\n"
+        with patch('sys.stdout', new=StringIO()) as str_out:
+            r1.display()
+        self.assertEqual(str_out.getvalue(), res)
+
+        r1 = Square(3, 1, 3)
+        res = "[Square] (22) 1/3 - 3\n"
+        with patch('sys.stdout', new=StringIO()) as str_out:
+            print(r1)
+        self.assertEqual(str_out.getvalue(), res)
+
+        self.assertEqual(r1.area(), 9)
+
+        res = "\n\n\n ###\n ###\n ###\n"
+        with patch('sys.stdout', new=StringIO()) as str_out:
+            r1.display()
+        self.assertEqual(str_out.getvalue(), res)
+
         r2 = Square(2, 10, 0, 6)
         self.assertEqual(r2.id, 6)
         r3 = Square(10, 2, 0, 7)
