e7322f78e4278622cf71bd77a29adad0aa3b7480
MahmoudEasa <mu01011422865@gmail.com>
2023-07-18 17:36:53 +0000

    Fix base.py 3

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -92,9 +92,9 @@ class Base:
         """
 
         if cls.__name__ == "Rectangle":
-            dummy = cls(1, 1, 0, 0, 99999)
+            dummy = cls(1, 1, 0, 0)
         else:
-            dummy = cls(1, 0, 0, 9999)
+            dummy = cls(1)
 
         dummy.update(**dictionary)
 
