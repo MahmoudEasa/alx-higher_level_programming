25fa9d1281d6759e1139c23ed7cd753b1181246a
MahmoudEasa <mu01011422865@gmail.com>
2023-08-16 22:17:24 +0000

    16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- Lists all shows, and all genres linked to that show,
+-- from the database hbtn_0d_tvshows
+SELECT s.title,
+	COALESCE(g.name, NULL) AS name
+FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS sg ON s.id = sg.show_id
+LEFT JOIN tv_genres AS g ON sg.genre_id = g.id
+ORDER BY s.title, g.name ASC;
+
