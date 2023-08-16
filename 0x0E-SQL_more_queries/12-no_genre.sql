f9e14cd9cdb509b020bcedb08e4d12e5f20e53fb
MahmoudEasa <mu01011422865@gmail.com>
2023-08-16 21:23:52 +0000

    12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,9 @@
+-- Lists all shows contained in hbtn_0d_tvshows without a genre linked
+SELECT tv_shows.title,
+	COALESCE(tv_show_genres.genre_id, NULL) AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+
