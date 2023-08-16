792f6711984ee7b7b440e062731f29c0ab5dc924
MahmoudEasa <mu01011422865@gmail.com>
2023-08-16 21:12:54 +0000

    11-genre_id_all_shows.sql

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,8 @@
+-- Lists all shows contained in the database hbtn_0d_tvshows
+SELECT tv_shows.title,
+	COALESCE(tv_show_genres.genre_id, NULL) AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+
