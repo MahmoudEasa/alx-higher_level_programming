e693ba9400f6f66f70c3f070183d52009472ef09
MahmoudEasa <mu01011422865@gmail.com>
2023-09-12 11:39:13 -0700

    1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor(w, h) {
-    this.whidth = w;
-    this.whidth = w;
-  };
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 };
