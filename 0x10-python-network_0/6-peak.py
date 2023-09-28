0bb334a6411d7e625c74114af7807a4f284fc811
MahmoudEasa <mu01011422865@gmail.com>
2023-09-28 20:45:14 +0000

    6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers
+    """
+
+    if type(list_of_integers) is not list:
+        return (None)
+
+    list_len = len(list_of_integers)
+    if list_len == 0:
+        return (None)
+
+    if list_len < 3:
+        return (max(list_of_integers))
+
+    list_of_integers.sort()
+    return (list_of_integers[-1])
