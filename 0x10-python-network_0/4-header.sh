b01f490e190498fb4655b58a34b4bc6cc20fbe7e
MahmoudEasa <mu01011422865@gmail.com>
2023-09-28 19:52:15 +0000

    Fix 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sGd "X-School-User-Id=98" $1
+curl -sX GET -H "X-School-User-Id=98" $1
