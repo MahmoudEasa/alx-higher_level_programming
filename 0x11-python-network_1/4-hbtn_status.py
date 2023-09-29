6c88308628bb1a3cdb557aaedfc3d1dc932784f5
MahmoudEasa <mu01011422865@gmail.com>
2023-09-29 01:47:29 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-"""
+"""Fetches https://alx-intranet.hbtn.io/status
 """
 
+import requests
 
+req = requests.get("https://alx-intranet.hbtn.io/status")
+
+output = f"""Body response:
+    - type: {type(req.text)}
+    - content: {req.text}"""
+
+print(output)
