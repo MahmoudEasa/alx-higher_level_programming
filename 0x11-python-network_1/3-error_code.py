1878b102b88b4bfac93cee539c9e39eca5fe266d
MahmoudEasa <mu01011422865@gmail.com>
2023-09-29 01:14:32 -0700

    3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
-"""
+"""Sends a request to the URL and displays the body
+    of the response (decoded in utf-8)
 """
 
+import urllib.error as err
+import urllib.request as req
+import sys
 
+url = sys.argv[1]
+
+re = req.Request(url)
+
+try:
+    with req.urlopen(re) as res:
+        data = res.read()
+        print(data.decode())
+except err.HTTPError as e:
+    print(f"Error code: {e.getcode()}")
