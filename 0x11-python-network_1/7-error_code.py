94a70d2946ee758b22333ab2246866ab160c9290
MahmoudEasa <mu01011422865@gmail.com>
2023-09-29 02:01:56 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-"""
+"""Sends a request to the URL and displays the body of the response
 """
 
+import requests
+import sys
 
+url = sys.argv[1]
+
+req = requests.get(url)
+
+if req.ok:
+    print(req.text)
+else:
+    print(f"Error code: {req.status_code}")
