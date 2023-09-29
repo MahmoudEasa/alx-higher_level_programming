311f3e85376c13358cc1338b43cc69e2d95896d0
MahmoudEasa <mu01011422865@gmail.com>
2023-09-29 02:14:59 -0700

    Fix 0-hbtn_status.py 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,11 +8,7 @@ if __name__ == "__main__":
 
     with req.urlopen('https://alx-intranet.hbtn.io/status') as res:
         data = res.read()
-        type_req = type(data)
-        utf8 = data.decode()
-        output = f"""Body response:
-    - type: {type_req}
-    - content: {data}
-    - utf8 content: {utf8}"""
-
-        print(output)
+        print("Body response:")
+        print(f"\t- type: {type(data)}")
+        print(f"\t- content: {data}")
+        print(f"\t- utf8 content: {data.decode()}")
