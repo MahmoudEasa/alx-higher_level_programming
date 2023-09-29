c0638b5142ff3481bfef8da19693a0d4de28e0c2
MahmoudEasa <mu01011422865@gmail.com>
2023-09-29 07:08:28 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
-"""
+""" Fetches https://alx-intranet.hbtn.io/status
 """
 
+import urllib.request as req
 
+with req.urlopen('https://alx-intranet.hbtn.io/status') as res:
+    data = res.read()
+    type_req = type(data)
+    utf8 = data.decode()
+    output = f"""Body response:
+    - type: {type_req}
+    - content: {data}
+    - utf8 content: {utf8}"""
+
+    print(output)
