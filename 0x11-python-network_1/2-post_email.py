d89343e2739e36dc9788e5442e378133be7bd4fe
MahmoudEasa <mu01011422865@gmail.com>
2023-09-29 02:18:53 -0700

    Fix 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,20 +3,22 @@
     and displays the body of the response (decoded in utf-8)
 """
 
-import urllib.request as req
-import urllib.parse as parse
-import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+if __name__ == "__main__":
+    import urllib.request as req
+    import urllib.parse as parse
+    import sys
 
-values = {"email": email}
+    url = sys.argv[1]
+    email = sys.argv[2]
 
-data_req = parse.urlencode(values)
-data_req = data_req.encode('utf-8')
+    values = {"email": email}
 
-re = req.Request(url, data_req)
+    data_req = parse.urlencode(values)
+    data_req = data_req.encode('utf-8')
 
-with req.urlopen(re) as res:
-    data = res.read()
-    print(data.decode())
+    re = req.Request(url, data_req)
+
+    with req.urlopen(re) as res:
+        data = res.read()
+        print(data.decode())
