9929199d22e7222ec0c7fc82016266619e5c8593
MahmoudEasa <mu01011422865@gmail.com>
2023-09-29 03:02:48 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
-"""
+"""Takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the letter as a parameter
 """
 
 if __name__ == "__main__":
     import requests
     import sys
 
-    url = sys.argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    letter = ""
 
-    req = requests.get(url)
+    if len(sys.argv) == 2:
+        letter = sys.argv[1]
+
+    req = requests.post(url, data={"q": letter})
+
+    if not req.json():
+        print("No result")
+    else:
+        data = req.json()
+        print(f"[{data['id']}] {data['name']}")
