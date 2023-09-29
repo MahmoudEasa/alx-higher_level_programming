2977d42ef41a2f9f469a9685d72e0356c0089c3a
MahmoudEasa <mu01011422865@gmail.com>
2023-09-29 21:18:01 +0000

    2~

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""
+"""List 10 commits (from the most recent to oldest)
+    of the repository “rails” by the user “rails”
 """
 
 if __name__ == "__main__":
     import requests
-    import sys
+    from sys import argv
 
-    url = sys.argv[1]
-
+    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
     req = requests.get(url)
+    commits = req.json()
+
+    try:
+        for i in range(10):
+            print("{}: {}".format(commits[i].get('sha'),
+                  commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
