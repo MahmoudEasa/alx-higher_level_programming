13a7a0a0c3b1132d0b4e9897f270038baccc7675
MahmoudEasa <mu01011422865@gmail.com>
2023-10-24 20:52:14 +0000

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (err, res, body) => {
+  console.log(err || JSON.parse(body).title);
+});
