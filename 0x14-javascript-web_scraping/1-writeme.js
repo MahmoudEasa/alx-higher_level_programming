1b08374028d0500fb10ad71b4ee6a735240886c8
MahmoudEasa <mu01011422865@gmail.com>
2023-10-24 20:32:00 +0000

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+const text = process.argv[3];
+
+fs.writeFile(file, text, 'utf8', function (err) {
+  if (err) console.log(err);
+});
