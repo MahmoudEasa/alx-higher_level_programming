20b9649688e869abf18bb8febb883f95dd70e6fc
MahmoudEasa <mu01011422865@gmail.com>
2023-10-24 21:11:41 +0000

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  const results = JSON.parse(body).results;
+  let count = 0;
+  for (let i = 0; i < results.length; i++) {
+    for (let j = 0; j < results[i].characters.length; j++) {
+      if (results[i].characters[j] === 'https://swapi-api.alx-tools.com/api/people/18/') {
+        count++;
+      }
+    }
+  }
+  console.log(err || count);
+});
