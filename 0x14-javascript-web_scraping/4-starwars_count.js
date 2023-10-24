3a745c427fb07415945b90dd35db8a8eb6627b8c
MahmoudEasa <mu01011422865@gmail.com>
2023-10-24 21:14:14 +0000

    Fix 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,14 +4,18 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (err, res, body) => {
-  const results = JSON.parse(body).results;
-  let count = 0;
-  for (let i = 0; i < results.length; i++) {
-    for (let j = 0; j < results[i].characters.length; j++) {
-      if (results[i].characters[j] === 'https://swapi-api.alx-tools.com/api/people/18/') {
-        count++;
+  if (err) {
+    console.log(err);
+  } else {
+    const results = JSON.parse(body).results;
+    let count = 0;
+    for (let i = 0; i < results.length; i++) {
+      for (let j = 0; j < results[i].characters.length; j++) {
+        if (results[i].characters[j] === 'https://swapi-api.alx-tools.com/api/people/18/') {
+          count++;
+        }
       }
     }
+    console.log(count);
   }
-  console.log(err || count);
 });
