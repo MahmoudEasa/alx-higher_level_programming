3e5da308a2985ccefc4a9dd32dc6bddf3428b3e7
MahmoudEasa <mu01011422865@gmail.com>
2023-10-25 15:06:23 +0000

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    const result = {};
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].completed) {
+        if (result[data[i].userId]) {
+          result[data[i].userId]++;
+        } else {
+          result[data[i].userId] = 1;
+        }
+      }
+    }
+    console.log(result);
+  }
+});
