1b23e7e63e69735c67c5c5ee920207bb6e074715
MahmoudEasa <mu01011422865@gmail.com>
2023-10-25 14:53:11 +0000

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.writeFile(file, body, 'utf8', function (err) {
+      if (err) console.log(err);
+    });
+  }
+});
