3fb183d6f154d7018da4110e66169fb50884ea9d
MahmoudEasa <mu01011422865@gmail.com>
2023-10-25 15:56:49 +0000

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body).characters;
+    for (let i = 0; i < data.length; i++) {
+      request(data[i], (err, res, body) => {
+        console.log(err || JSON.parse(body).name);
+      });
+    }
+  }
+});
